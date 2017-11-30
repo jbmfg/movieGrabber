c754a3f13e7323265872fe4ed27668a4ce0fa819
Ben Goff <bengoff@gmail.com>
2017-11-30 15:00:28 -0500

    complete rewrite with requests

--- movieGrabber.py
+++ movieGrabber.py
@@ -1,54 +1,36 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.5
 
-__author__ = 'GLXJBG0'
-import urllib.request
+import requests, json
 import urllib.parse
-import json
 
 def getConfigs():
-    with open("config.json", "r") as f:
+    with open("/home/jbg/.scripts/movieGrabberConfig.json", "r") as f:
         params = json.load(f)
     f.close()
     return params
 
-def create_url(params):
-    api_key = params["api"]
-    search_term = input('What movie are you looking for?\n')
-    search_term = search_term.replace(" ", "%20")
-    url = 'http://nzbs.org/api?apikey=' + api_key + '&o=json&t=search&cat=2000&q=' + search_term + '&maxage=1000'
-    return url
 
+def nzbsSearch(movie, apiKey):
+    url = 'http://nzbs.org/api?apikey=' + apiKey + '&o=json&t=search&cat=2000&q=' + movie + '&maxage=1000'
+    url = url.replace(" ", "%20")
+    headers = {"User-Agent":"python requests"}
+    results = requests.get(url, headers=headers)
+    return results.json()
 
-def get_movies(url):
-    r = urllib.request.urlopen(url)
-    r = r.read().decode('utf-8')
-    movies = json.loads(r)
-    movie_dict = []
-    for items in movies['channel']['item']:
-        movie_dict.append({'Title': items['title'],
-                           'category': items['category'],
-                           'size': items['attr'][2]['@attributes']['value'],
-                           'url': items['enclosure']['@attributes']['url']})
-    return movie_dict
+def send2sab(url, movie, sab_key, sab_addr):
+    sendUrl = "http://{0:s}:8080/sabnzbd/api?apikey={1:s}&mode=addurl&name={2:s}&cat=movies&priority=-1&nzbname={3:s}"\
+            .format(sab_addr, sab_key, urllib.parse.quote_plus(url), movie)
+    return requests.get(sendUrl)
 
-
-def send2sab(url, title, params):
-    sab_apikey = params["sab_key"]
-    url = params["sab_address"] + '/sabnzbd/api?apikey=' \
-            + sab_apikey + '&mode=addurl&name=' + url \
-            + '&cat=movies&priority=-1&nzbname=' + title
-    r = urllib.request.urlopen(url)
-    r.read()
-
-
-def main():
-    configs = getConfigs()
-    movies = get_movies(create_url(configs))
-    for x, i in enumerate(movies):
-        print(x, ' - ', i['Title'], i['category'], ' - ', str(round(int(i['size'])/1024000000, 2))+'Gb')
-    selection = int(input('Which would you like to download?\n'))
-    nzb = str(movies[selection]['url'])
-    nzb = urllib.parse.quote_plus(nzb)
-    send2sab(nzb, movies[selection]['Title'], configs)
-
-main()
+def interface():
+    params = getConfigs()
+    movie = input("What movie are you looking for?\n")
+    results = nzbsSearch(movie, params["api"])
+    availMovies = [[i["title"], i["link"], int(i["enclosure"]["@attributes"]["length"])] for i in results["channel"]["item"]]
+    # str(round(int(i['size'])/1024000000, 2))+'Gb')
+    for x,i in enumerate(availMovies):
+        print ("{0:d} - {1:s} - {2:s}".format(x, i[0], str(round(i[2]/1024000000, 2))))
+    movieNo = int(input("Which would you like to download?\n"))
+    return send2sab(availMovies[movieNo][1], availMovies[movieNo][0], params["sab_key"], params["sab_address"])
+	
+interface()
